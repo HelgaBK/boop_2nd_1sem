6074c4d6614c54032e37e0ceaccdcf7cd5cac2fb
Olia <olga.karpyshyn@gmail.com>
2018-11-20 19:44:02 +0200

    Completed SingleNoteView class

--- Notes/singlenoteview.cpp
+++ Notes/singlenoteview.cpp
@@ -1,6 +1,9 @@
 #include "singlenoteview.h"
 #include "ui_singlenoteview.h"
 
+#include <QPainter>
+#include <QStyleOption>
+
 SingleNoteView::SingleNoteView(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::SingleNoteView) {
@@ -12,17 +15,23 @@ SingleNoteView::~SingleNoteView() {
 }
 
 void SingleNoteView::setNote(NoteClass *note) {
-
+    this->note = note;
+    update();
 }
 
 void SingleNoteView::update() {
-
+    // TODO :: Add parsing text
+    ui->text->setText(note->getText());
+    ui->editedTime->setText(note->getEditedTime().toString(Qt::TextDate));
 }
 
 int SingleNoteView::getID() {
-
+    return this->note->getID();
 }
 
 void SingleNoteView::paintEvent(QPaintEvent *) {
-
+    QStyleOption opt;
+    opt.init(this);
+    QPainter *p = new QPainter(this);
+    style()->drawPrimitive(QStyle::PE_Widget, &opt, p, this);
 }
